9986b3bb671bd8db5239b8c71933df4ac184ab7f
9812598 <9812598@gmail.com>
2023-05-07 12:01:17 +0300

    make a carousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "@/styles/globals.css";
 import Layout from "../components/layout";
+import { Roboto } from "next/font/google";
+import "tw-elements/dist/css/tw-elements.min.css";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,19 @@ import MainMenu from "@/components/mainMenu";
 import Mission from "@/components/mission";
 import TernKey from "@/components/ternkey";
 import MapAndContact from "@/components/mapandcontact";
+import dynamic from "next/dynamic";
+
+const MyCarousel = dynamic(() => import("@/components/carousel"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
-    <div className="min-h-screen z-50">
+    <div className="min-h-screen ">
       <MainMenu />
       <Mission />
       <TernKey />
+      <MyCarousel />
       <MapAndContact />
     </div>
   );
